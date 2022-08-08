e01bfa168b6aad73e6402922100225c00671659c
zzy <zzy@zzydeMac-mini.local>
2022-08-08 11:33:34 +0800

    add compute winner

--- src/index.js
+++ src/index.js
@@ -33,26 +33,43 @@ class Board extends React.Component {
     }
 
     renderSquare(i) {
-        return <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)} 
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
         />;
     }
 
-    handleClick(i){
+    handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext?'X':'O';
+
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
         // squares[i] = 'X';
-        
-        this.setState({squares:squares,xIsNext:!this.state.xIsNext});
-        
+        console.log(this.state);
+        this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+
         // this.state.squares[i] = 'X';
         // this.setState(this.state);
         // console.log(this.state)
     }
 
     render() {
-        const status = 'Next player: X';
+
+        const winner = calculateWinner(this.state.squares);
+        let status;
+
+        // const status = 'Next player: ' + (this.state.xIsNext?'X':'O');
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
 
         return (
             <div>
@@ -97,3 +114,24 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
